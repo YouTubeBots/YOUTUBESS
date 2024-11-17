eba2b6ec09329f9f04d2d737b0a86adeea64e10c
youtube <cookiesfile4@gmail.com>
2024-11-17 20:30:51 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -92,7 +92,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamVivek/YukkiMusic",
+    "https://github.com/YouTubeBots/YOUTUBESS",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
