8b7cfd61135cf2b293965bd161a3eff066621978
youtube <cookiesfile4@gmail.com>
2024-11-18 16:46:56 +0530

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -1,118 +1,101 @@
-#
-# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
-#
-# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the MIT License.
-# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
-#
+# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, <https://github.com/THE-VIP-BOY-OP>.
+# This file is part of <https://github.com/THE-VIP-BOY-OP/VIP-MUSIC> project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see <https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE>
 # All rights reserved.
-#
+
+import asyncio
+import threading
+
 import uvloop
-
-uvloop.install()
-
-
-import sys
-
-from pyrogram import Client
+from flask import Flask
+from pyrogram import Client, idle
 from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import (
-    ChatSendPhotosForbidden,
-    ChatWriteForbidden,
-    FloodWait,
-    MessageIdInvalid,
-)
 from pyrogram.types import (
     BotCommand,
     BotCommandScopeAllChatAdministrators,
     BotCommandScopeAllGroupChats,
     BotCommandScopeAllPrivateChats,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
 )
 
 import config
 
 from ..logging import LOGGER
 
+uvloop.install()
 
+# Flask app initialize
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+
+def run():
+    app.run(host="0.0.0.0", port=8000, debug=False)
+
+
+# YukkiBot Class
 class YukkiBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot")
+        LOGGER(__name__).info("Starting Bot")
         super().__init__(
             "YukkiMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            sleep_threshold=240,
-            max_concurrent_transmissions=5,
-            workers=50,
         )
-
-    async def edit_message_text(self, *args, **kwargs):
-        try:
-            return await super().edit_message_text(*args, **kwargs)
-        except FloodWait as e:
-            time = int(e.value)
-            await asyncio.sleep(time)
-            if time < 25:
-                return await self.edit_message_text(self, *args, **kwargs)
-        except MessageIdInvalid:
-            pass
-
-    async def send_message(self, *args, **kwargs):
-        if kwargs.get("send_direct", False):
-            kwargs.pop("send_direct", None)
-            return await super().send_message(*args, **kwargs)
-
-        try:
-            return await super().send_message(*args, **kwargs)
-        except FloodWait as e:
-            time = int(e.value)
-            await asyncio.sleep(time)
-            if time < 25:
-                return await self.send_message(self, *args, **kwargs)
-        except ChatWriteForbidden:
-            chat_id = kwargs.get("chat_id") or args[0]
-            if chat_id:
-                await self.leave_chat(chat_id)
-
-    async def send_photo(self, *args, **kwargs):
-        try:
-            return await super().send_photo(*args, **kwargs)
-        except FloodWait as e:
-            time = int(e.value)
-            await asyncio.sleep(time)
-            if time < 25:
-                return await self.send_photo(self, *args, **kwargs)
-        except ChatSendPhotosForbidden:
-            chat_id = kwargs.get("chat_id") or args[0]
-            if chat_id:
-                await self.send_message(
-                    chat_id,
-                    "I don't have the right to send photos in this chat, leaving now..",
-                )
-                await self.leave_chat(chat_id)
 
     async def start(self):
         await super().start()
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.mention = self.me.mention
+        self.name = get_me.first_name + " " + (get_me.last_name or "")
+        self.mention = get_me.mention
 
-        try:
-            await self.send_message(
-                config.LOG_GROUP_ID,
-                text=f"<u><b>{self.mention} Bot Started :</b><u>\n\nId : <code>{self.id}</code>\nName : {self.name}\nUsername : @{self.username}",
+        button = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="๏ ᴀᴅᴅ ᴍᴇ ɪɴ ɢʀᴏᴜᴘ ๏",
+                        url=f"https://t.me/{self.username}?startgroup=true",
+                    )
+                ]
+            ]
+        )
+
+        if config.LOG_GROUP_ID:
+            try:
+                await self.send_photo(
+                    config.LOG_GROUP_ID,
+                    photo=config.START_IMG_URL,
+                    caption=f"╔════❰𝐖𝐄𝐋𝐂𝐎𝐌𝐄❱════❍⊱❁۪۪\n║\n║┣⪼🥀𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐁𝐚𝐛𝐲🎉\n║\n║┣⪼ {self.name}\n║\n║┣⪼🎈𝐈𝐃:- `{self.id}` \n║\n║┣⪼🎄@{self.username} \n║ \n║┣⪼💖𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠😍\n║\n╚════════════════❍⊱❁",
+                    reply_markup=button,
+                )
+            except pyrogram.errors.ChatWriteForbidden as e:
+                LOGGER(__name__).error(f"Bot cannot write to the log group: {e}")
+                try:
+                    await self.send_message(
+                        config.LOG_GROUP_ID,
+                        f"╔═══❰𝐖𝐄𝐋𝐂𝐎𝐌𝐄❱═══❍⊱❁۪۪\n║\n║┣⪼🥀𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭𝐞𝐝 𝐁𝐚𝐛𝐲🎉\n║\n║◈ {self.name}\n║\n║┣⪼🎈𝐈𝐃:- `{self.id}` \n║\n║┣⪼🎄@{self.username} \n║ \n║┣⪼💖𝐓𝐡𝐚𝐧𝐤𝐬 𝐅𝐨𝐫 𝐔𝐬𝐢𝐧𝐠😍\n║\n╚══════════════❍⊱❁",
+                        reply_markup=button,
+                    )
+                except Exception as e:
+                    LOGGER(__name__).error(f"Failed to send message in log group: {e}")
+            except Exception as e:
+                LOGGER(__name__).error(
+                    f"Unexpected error while sending to log group: {e}"
+                )
+        else:
+            LOGGER(__name__).warning(
+                "LOG_GROUP_ID is not set, skipping log group notifications."
             )
-        except:
-            LOGGER(__name__).error(
-                "Bot has failed to access the log group. Make sure that you have added your bot to your log channel and promoted as admin!"
-            )
-            LOGGER(__name__).error("An error occurred", exc_info=True)
-            sys.exit()
-        if config.SET_CMDS == str(True):
+        if config.SET_CMDS:
             try:
                 await self.set_bot_commands(
                     commands=[
@@ -125,44 +108,82 @@ class YukkiBot(Client):
                 await self.set_bot_commands(
                     commands=[
                         BotCommand("play", "Start playing requested song"),
+                        BotCommand("stop", "Stop the current song"),
+                        BotCommand("pause", "Pause the current song"),
+                        BotCommand("resume", "Resume the paused song"),
+                        BotCommand("queue", "Check the queue of songs"),
+                        BotCommand("skip", "Skip the current song"),
+                        BotCommand("volume", "Adjust the music volume"),
+                        BotCommand("lyrics", "Get lyrics of the song"),
                     ],
                     scope=BotCommandScopeAllGroupChats(),
                 )
                 await self.set_bot_commands(
                     commands=[
-                        BotCommand("play", "Start playing requested song"),
-                        BotCommand("skip", "Move to next track in queue"),
-                        BotCommand("pause", "Pause the current playing song"),
-                        BotCommand("resume", "Resume the paused song"),
-                        BotCommand("end", "Clear the queue and leave voicechat"),
-                        BotCommand("shuffle", "Randomly shuffles the queued playlist."),
-                        BotCommand(
-                            "playmode",
-                            "Allows you to change the default playmode for your chat",
-                        ),
-                        BotCommand(
-                            "settings",
-                            "Open the settings of the music bot for your chat.",
-                        ),
+                        BotCommand("start", "❥ Start the bot"),
+                        BotCommand("ping", "❥ Check the ping"),
+                        BotCommand("help", "❥ Get help"),
+                        BotCommand("vctag", "❥ Tag all for voice chat"),
+                        BotCommand("stopvctag", "❥ Stop tagging for VC"),
+                        BotCommand("tagall", "❥ Tag all members by text"),
+                        BotCommand("cancel", "❥ Cancel the tagging"),
+                        BotCommand("settings", "❥ Get the settings"),
+                        BotCommand("reload", "❥ Reload the bot"),
+                        BotCommand("play", "❥ Play the requested song"),
+                        BotCommand("vplay", "❥ Play video along with music"),
+                        BotCommand("end", "❥ Empty the queue"),
+                        BotCommand("playlist", "❥ Get the playlist"),
+                        BotCommand("stop", "❥ Stop the song"),
+                        BotCommand("lyrics", "❥ Get the song lyrics"),
+                        BotCommand("song", "❥ Download the requested song"),
+                        BotCommand("video", "❥ Download the requested video song"),
+                        BotCommand("gali", "❥ Reply with fun"),
+                        BotCommand("shayri", "❥ Get a shayari"),
+                        BotCommand("love", "❥ Get a love shayari"),
+                        BotCommand("sudolist", "❥ Check the sudo list"),
+                        BotCommand("owner", "❥ Check the owner"),
+                        BotCommand("update", "❥ Update bot"),
+                        BotCommand("gstats", "❥ Get stats of the bot"),
+                        BotCommand("repo", "❥ Check the repo"),
                     ],
                     scope=BotCommandScopeAllChatAdministrators(),
                 )
-            except:
-                pass
-        else:
-            pass
-        try:
-            a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-            if a.status != ChatMemberStatus.ADMINISTRATOR:
-                LOGGER(__name__).error("Please promote bot as admin in logger group")
-                sys.exit()
-        except Exception:
-            pass
-        if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
-        else:
-            self.name = get_me.first_name
-        LOGGER(__name__).info(f"MusicBot started as {self.name}")
+            except Exception as e:
+                LOGGER(__name__).error(f"Failed to set bot commands: {e}")
 
-    async def stop(self):
-        await super().stop()
+        if config.LOG_GROUP_ID:
+            try:
+                chat_member_info = await self.get_chat_member(
+                    config.LOG_GROUP_ID, self.id
+                )
+                if chat_member_info.status != ChatMemberStatus.ADMINISTRATOR:
+                    LOGGER(__name__).error(
+                        "Please promote Bot as Admin in Logger Group"
+                    )
+            except Exception as e:
+                LOGGER(__name__).error(f"Error occurred while checking bot status: {e}")
+
+        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+
+
+# Define the async boot function
+async def anony_boot():
+    bot = VIPBot()
+    await bot.start()
+    await idle()
+
+
+if __name__ == "__main__":
+    LOGGER(__name__).info("Starting Flask server...")
+
+    # Start Flask server in a new thread
+    t = threading.Thread(target=run)
+    t.daemon = True
+    t.start()
+
+    LOGGER(__name__).info("Starting VIPBot...")
+
+    # Run the bot
+    asyncio.run(anony_boot())
+
+    LOGGER(__name__).info("Stopping VIPBot...")
