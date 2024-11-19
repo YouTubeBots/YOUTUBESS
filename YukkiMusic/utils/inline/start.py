000b27730eb41662c580defb6556f52c7dbc199b
youtube <cookiesfile4@gmail.com>
2024-11-19 19:21:07 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -85,6 +85,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
                     InlineKeyboardButton(text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/BRANDED_WORLD"
                 ),
-            )
+            ]
     buttons.append([InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")])
     return buttons
